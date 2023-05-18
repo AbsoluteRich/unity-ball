8b4ca9f89f97a36bc7e4de8dcefe2be68172e2ff
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2023-05-18 10:32:21 +1000

    Day 5
    Finale
    ----
    Double on 18/05/2023

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,9 @@ public class PlayerController : MonoBehaviour
     private AudioSource explode;
     private string sceneName;
     public GameObject loseTextObject;
+    private AudioSource[] sfx;
+    private AudioSource powerup;
+    private bool hasWon;
 
     // Start is called before the first frame update
     void Start()
@@ -25,15 +28,19 @@ public class PlayerController : MonoBehaviour
         count = 0;
         SetCountText();
         winTextObject.SetActive(false);
-        explode = GetComponent<AudioSource>();
+        sfx = GetComponents<AudioSource>();
+        explode = sfx[0];
+        powerup = sfx[1];
         Scene currentScene = SceneManager.GetActiveScene();
         sceneName = currentScene.name;
         loseTextObject.SetActive(false);
+        hasWon = false;
     }
 
     // Called when a move event occurs
     void OnMove(InputValue movementValue)
     {
+        // Todo: Make the player's movement slow down to 0 after releasing the key
         Vector2 movementVector = movementValue.Get<Vector2>();
 
         movementX = movementVector.x;
@@ -55,6 +62,8 @@ public class PlayerController : MonoBehaviour
                     break;
 
                 case "Level2":
+                    powerup.Play();
+                    hasWon = true;
                     winTextObject.SetActive(true);
                     break;
             }
@@ -70,23 +79,41 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    // Called when the player dies after 2 seconds
+    private void Respawn()
+    {
+        loseTextObject.SetActive(false);
+        gameObject.transform.position = new Vector3(0f, 0.5f, 0f);
+        gameObject.SetActive(true);
+    }
+
     // Called when player collides with an object
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collided with " + other.gameObject.name);
+
+        // Todo: Make the win sound effect play instead of this when the player, well, wins
+        //  The winning effect plays *as* the player gets a pickup, add logic where if the count is above a certain point then play the sound effect
+        explode.Play();
+
         if (other.gameObject.CompareTag("PickUp")) // When the player collides with a pick up
         {
-            explode.Play();
             other.gameObject.SetActive(false);
             count = count + 1;
             SetCountText();
         }
-        else if (other.gameObject.CompareTag("Walls")) // When the player collides with a wall
+        else if (other.gameObject.CompareTag("Walls")) // When the player collides with a red wall
         {
-            // Todo: Hide the player, lock inputs, and play a sound effect
-            //  Pickups should not be collidable when the player has lost
-            gameObject.SetActive(false);  // THIS JUST WORKS!!
-            loseTextObject.SetActive(true);
+            if (!hasWon)
+            {
+                gameObject.SetActive(false);
+                loseTextObject.SetActive(true);
+                Invoke("Respawn", 2.0f);
+            }
+            else
+            {
+                gameObject.transform.position = new Vector3(0f, 0.5f, 0f);
+            }
         }
     }
 }
